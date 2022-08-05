78e4080f98b74011508fe670dbb7d3ae6437726a
Georg Krause <mail@georg-krause.net>
2022-08-05 17:36:00 +0200

    Only use graphical login for now

--- front/cypress/integration/login.spec.js
+++ front/cypress/integration/login.spec.js
@@ -10,18 +10,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('The login', () => {
 
   it('is working with UI', () => {
-    cy.visit('/login')
-    cy.get('input[name=username]').type(username)
-    cy.get('input[name=password]').type(`${password}{enter}`)
-    cy.url().should('include', '/library')
-
-    cy.getCookie('sessionid').should('exist')
-  })
-
-  it('is working without UI', () => {
     cy.login(username, password)
-    cy.visit('/library')
-    cy.get('nav.top').children().should('have.length', 7)
+
+    cy.url().should('include', '/library')
     cy.getCookie('sessionid').should('exist')
   })
+
+//  it('is working without UI', () => {
+//    cy.login(username, password)
+//    cy.visit('/library')
+//    cy.get('.ui.avatar.circular.label').should('exist')
+//    cy.getCookie('sessionid').should('exist')
+//  })
 })
--- front/cypress/support/commands.js
+++ front/cypress/support/commands.js
@@ -1,21 +1,29 @@
-Cypress.Commands.add('login', (username, password) => {
-  cy.request('/login')
-  cy.getCookie('csrftoken').then(($cookie) => {
-    const csrfToken = $cookie.value
-    cy.log(csrfToken)
+// Currently we cannot login purely programatically, so we need to use the
+// graphical login until the vue3 branch is merged
+//Cypress.Commands.add('login', (username, password) => {
+//  cy.request('/login')
+//  cy.getCookie('csrftoken').then(($cookie) => {
+//    const csrfToken = $cookie.value
+//    cy.log(csrfToken)
+//
+//    cy.request({
+//      method: 'POST',
+//      url: '/api/v1/users/login',
+//      form: true,
+//      headers: {
+//        'X-CSRFTOKEN': csrfToken,
+//        Referer: Cypress.config().baseUrl + '/login',
+//      },
+//      body: {
+//        username,
+//        password
+//      },
+//    })
+//  })
+//})
 
-    cy.request({
-      method: 'POST',
-      url: '/api/v1/users/login',
-      form: true,
-      headers: {
-        'X-CSRFTOKEN': csrfToken,
-        Referer: Cypress.config().baseUrl + '/login',
-      },
-      body: {
-        username,
-        password
-      },
-    })
-  })
+Cypress.Commands.add('login', (username, password) => {
+  cy.visit('/login')
+  cy.get('input[name=username]').type(username)
+  cy.get('input[name=password]').type(`${password}{enter}`)
 })
